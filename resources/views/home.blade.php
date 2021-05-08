0e5707c7e0c3d451485c2365639f5d027764eb06
n-desantis92 <dsnwebdev@gmail.com>
2021-05-08 15:15:03 +0200

    aggiunto logo e nav menu con lista e tag a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,18 @@
     <title>La Molisana</title>
 </head>
 <body>
-    <h1>ciao</h1>
+    <header>
+        <div class="logo">
+            <img src="{{ asset('../images/marchio-sito-test.png') }}" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href=""#>Home</a></li>
+                <li><a href="#">Prodotti</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </nav>
+    </header>
+
 </body>
 </html>
