e6d642dad8accc1ce3ac7f571fc3da982a0fe067
n-desantis92 <dsnwebdev@gmail.com>
2021-05-10 19:47:44 +0200

    aggiunte chevron a prodotti

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -15,7 +15,11 @@
 
             <h1>{{$pasta['titolo']}}</h1>
             <img src="{{$pasta['src-h']}}" alt="">
-            <img src="{{$pasta['src-p']}}" alt="{{$pasta['titolo']}}">
+            <div class="slider">
+                <a href="#"><i class="fas fa-chevron-left"></i></a>
+                <img src="{{$pasta['src-p']}}" alt="{{$pasta['titolo']}}">
+                <a href="#"><i class="fas fa-chevron-right"></i></a>
+            </div>
             <p>
                 {!!$pasta['descrizione']!!}
             </p>
